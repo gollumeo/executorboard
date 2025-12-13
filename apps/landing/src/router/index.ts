fd0bc03d0bef639a445dc2a1034a8eeafd36a03d
Pierre "Golluméo" Mauriello <hire-me@pierre-mauriello.be>
2025-12-13 19:49:50 +0100

    chore(router): format imports and standardize route definitions in `index.ts`

--- apps/landing/src/router/index.ts
+++ apps/landing/src/router/index.ts
@@ -1,24 +1,27 @@
-import { createMemoryHistory, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import {
+  createMemoryHistory,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
 
 export const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import('../pages/index.vue'),
+    path: "/",
+    name: "home",
+    component: () => import("../pages/index.vue"),
   },
   {
-    path: '/for-attorneys',
-    name: 'for-attorneys',
-    component: () => import('../pages/for-attorneys.vue'),
+    path: "/for-attorneys",
+    name: "for-attorneys",
+    component: () => import("../pages/for-attorneys.vue"),
   },
   {
-    path: '/guides/how-to-keep-heirs-updated',
-    name: 'guides-how-to-keep-heirs-updated',
-    component: () => import('../pages/guides/how-to-keep-heirs-updated.vue'),
+    path: "/guides/how-to-keep-heirs-updated",
+    name: "guides-how-to-keep-heirs-updated",
+    component: () => import("../pages/guides/how-to-keep-heirs-updated.vue"),
   },
-]
+];
 
-export const history =
-  import.meta.env.SSR
-    ? createMemoryHistory(import.meta.env.BASE_URL)
-    : createWebHistory(import.meta.env.BASE_URL)
+export const history = import.meta?.env?.SSR
+  ? createMemoryHistory(import.meta.env.BASE_URL)
+  : createWebHistory(import.meta.env.BASE_URL);
