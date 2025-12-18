2b8986c16a326290f647cd68fd30937d1a437b94
Pierre "Golluméo" Mauriello <hire-me@pierre-mauriello.be>
2025-12-18 18:04:16 +0100

    chore(router): add scroll behavior to reset position on navigation

--- apps/landing/src/router/index.ts
+++ apps/landing/src/router/index.ts
@@ -30,4 +30,7 @@ export const routes: RouteRecordRaw[] = [
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { left: 0, top: 0 };
+  },
 });
